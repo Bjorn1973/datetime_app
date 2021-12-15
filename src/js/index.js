8bbbfac8e50b4c32a291ab70c2e7cfd7c8b184f9
Bjorn Loyens <bjorn.loyens@gmail.com>
2021-12-15 20:32:43 +0100

    adding githubcorner

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,9 @@ import "../includes/svgxuse";
 import form from "./components/Form";
 import datetime from "./components/Datetime";
 import intro from "./components/Intro";
+import githubcorner from "./components/Githubcorner";
 
+const holderGit = document.querySelector("body");
 const holder = document.querySelector(".container");
 const formHolder = document.querySelector(".columns");
 const dateHolder = document.querySelector(".columns");
@@ -12,3 +14,4 @@ const dateHolder = document.querySelector(".columns");
 new intro(holder);
 new form(formHolder);
 new datetime(dateHolder);
+new githubcorner(holderGit);
